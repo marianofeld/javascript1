cca66fc58a86943558a6515ee1d4f4acd0ff670e
marianofeld <112523084+marianofeld@users.noreply.github.com>
2022-12-12 14:04:11 -0300

    segunda preentrega

--- js.js
+++ js.js
@@ -1,36 +1,62 @@
-// poner algo antes
+// let nombre = prompt("Cual es tu nombre?")
 
-let pregunta1 = ""
-let pregunta2 = ""
-let pregunta3 = ""
-let contador = 0
-let nombre = prompt("Cual es tu nombre?").toUpperCase()
+let menu = [{ nombre: "hamburguesa combo A", precio: 1500, vegana: false },
+{ nombre: "hamburguesa combo B", precio: 1900, vegana: false },
+{ nombre: "hamburguesa veggie", precio: 1850, vegana: true },
+{ nombre: "hamburguesa combo C", precio: 2000, vegana: false },
+{ nombre: "empanada de pollo", precio: 150, vegana: false },
+{ nombre: "empanada de verdura", precio: 150, vegana: true }
+]
+
+let pregunta
+let opcionVegano
+let opcionCarne
+let precio = 0
+
 
 do {
-    if (pregunta1 != 10 && contador < 3) {
-        pregunta1 = Number(prompt("Cuál es el resultado del siguiente calculo: [1-(3+4*2)]:(-1)?"))
-        if (pregunta1 != 10) {
-            contador++
-            if (contador === 3) { alert(nombre + " has desaprobado la autoevaluación.") }
+    let pregunta = Number(prompt("Indique\nopcion 1\nsi desea productos veganos \nopcion 2\nsi desea productos con carne\nopcion 3 \nfinalizar la compra"))
+    let vegano = menu.filter((producto) => (producto.vegana == true))
+    let carne = menu.filter((producto) => (producto.vegana == false))
+    if (pregunta == 1) {
+        vegano.forEach(alimento => {
+            console.log(alimento.nombre + alimento.precio)
+        });
+        for (let index = 0; index <= vegano.length; index++) {
+            alert("opcion " + (index + 1) + " " + vegano[index].nombre + " el precio es " + vegano[index].precio)
+            if (index == vegano.length - 1) {
+                let opcionVegano = prompt("Que opcion deseas pedir")
+                precio = vegano[opcionVegano-1].precio + precio
+            }
+
+            // aca termina de pedir vegano
+
         }
-            continue
-        }
-        else if (pregunta2 != 1) {
-            pregunta2 = Number(prompt("Cuál es el resultado de sen (0) + cos(0)?"))
-            if (pregunta2 != 1) {
-                contador++
-                if (contador === 3) { alert(nombre + " has desaprobado la autoevaluación.") }
-            }
-                continue
-            }
-        else if (pregunta3 != 2) {
-            pregunta3 = Number(prompt("Cuál es el resultado de ln (e)^2?"))
-            if (pregunta3 != 2) {
-                contador++
-                if (contador === 3) { alert(nombre + " has desaprobado la autoevaluación.") }
-            }
-                continue
-            }
+        
+    }else if (pregunta == 2) { 
+            carne.forEach(alimento => {
+                console.log(alimento.nombre + alimento.precio)
+            });
+            for (let index = 0; index <= carne.length; index++) {
+                console.log("opcion " + (index + 1) + " " + carne[index].nombre + " el precio es " + carne[index].precio)
+                if (index == carne.length - 1) {
+                    let opcionCarne = prompt("Que opcion deseas pedir")
+                    precio = carne[opcionCarne-1].precio + precio
+                }
+         }
+        
+         console.log(precio)
     }
-    while (contador < 3 && (pregunta1 != 10 || pregunta2 != 1 || pregunta3 != 2))
-        if (contador < 3) { alert(nombre + " has aprobado la autoevaluación.") }
+            // aca termina de pedir carne
+ } while (pregunta != 3)
+ if (pregunta == 3 ) {
+    alert("el precio a pagar es "+precio)
+ }
+
+
+
+
+
+
+
+// poner un alert el menu y que con 0 pidan una y asi y para agregar y al finalizar qeu sume el total
